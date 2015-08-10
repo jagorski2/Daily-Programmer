581f64d86749abaeae08105fac8e13946be12342
Jim Gorski <jgorski2@buffalo.edu>
2015-08-10 14:03:32 -0400

    Get Coord method rewritten to work on the challenge input.

--- Daily-Programmer/227Easy.cs
+++ Daily-Programmer/227Easy.cs
@@ -10,112 +10,85 @@ namespace Dailys
     {
         static void Main(string[] args)
         {
-            int size = Convert.ToInt32(Console.ReadLine());
+            UInt64 size = Convert.ToUInt64(Console.ReadLine());
             /* When coord is true its case 1 and we return the coordinant of of the given number,
              * else it is case 2 and return the number of the given coord. */
-            bool coord = false;
-            int x, y, retx, rety;
-            int counter = 1;
-            int direction = 1;
+            UInt64 retx, rety;
             /* 
                1 = right
                2 = up
                3 = left
                4 = down 
              */
+
             while (size % 2 != 1)
             {
                 Console.WriteLine("Size of square must be odd, try again.");
-                size = Convert.ToInt32(Console.ReadLine());
+                size = Convert.ToUInt64(Console.ReadLine());
             }
+            UInt64 center = (UInt64)Math.Floor((double)size / 2);
             string command = Console.ReadLine();
             if (command.Contains(' '))
             {
-                coord = true;
-                rety = Convert.ToInt32(command.Split(' ')[0]) -1;
-                retx = Convert.ToInt32(command.Split(' ')[1]) -1;
+                rety = Convert.ToUInt64(command.Split(' ')[0]) - 1;
+                retx = Convert.ToUInt64(command.Split(' ')[1]) - 1;
             }
             else
             {
-                retx = rety = 0;
+                get_coord(Convert.ToUInt64(command), size, center);
             }
-
-            int center = (int)Math.Floor((double)size/2);
-            x = y = center;
-            int[,] grid = new int[size,size];
-            grid[x, y] = counter++;
-            while (counter <= size * size)
-            {
-                switch (direction)
-                {                  
-                    case 1:
-                        y++;
-                        grid[x, y] = counter;
-                        if (grid[x-1,y] == 0)
-                        {
-                            direction = 2;
-                        }
-                        
-                        break;
-                    case 2:
-                        x--;
-                        grid[x, y] = counter;
-                        if (grid[x, y-1] == 0)
-                        {
-                            direction = 3;
-                        }
-                        break;
-                    case 3:
-                        y--;
-                        grid[x, y] = counter;
-                        
-                        if (grid[x + 1, y] == 0)
-                        {
-                            direction = 4;
-                        }
-                        break;
-                    case 4:
-                        x++;
-                        grid[x, y] = counter;
-                        if (grid[x, y + 1] == 0)
-                        {
-                            direction = 1;
-                        }
-                        
-                        break;
-                    default:
-                        break;
-                }
-                counter++;
-            }
-            if (coord)
-            {
-               Console.WriteLine(grid[retx, rety]); 
-            }
-            else
-            {
-                Print2DArray(grid, Convert.ToInt32(command));
-            }
+           
             Console.ReadLine();
         }
-        public static void Print2DArray<T>(T[,] matrix, int num)
-        {
-            // This Method created with the help of Stack Overflow question
-            // http://stackoverflow.com/questions/24094093/how-to-print-2d-array-to-console-in-c-sharp
-            // Question by Big Shooter:
-            // http://stackoverflow.com/users/3422169/big-shooter
-            // Answer by Rezo Megrelidze:
-            // http://stackoverflow.com/users/2204040/rezo-megrelidze
 
-            for (int i = 0; i < matrix.GetLength(1); i++)
+        private static void get_coord(UInt64 num, UInt64 size, UInt64 center)
+        {
+            bool rowFound = false;
+            UInt64 highsquare = 0;
+            UInt64 l = 1;
+            UInt64 r = 3;
+            UInt64 endcorner = 0;
+            UInt64 pad = 0;
+            while (!rowFound)
             {
-                for (int j = 0; j < matrix.GetLength(0); j++)
+
+
+                if ((l * l) < num && num < (r * r))
                 {
-                    if (Convert.ToInt32(matrix[i, j]) == num)
+                    for (UInt64 i = (l * l) + 1; i < (r * r) + 1; i++)
                     {
-                        Console.WriteLine("("+(j+1)+","+(i+1)+")");
+                        highsquare = r;
+                        pad = (size - r)/2;
+                        endcorner = r + pad;
+                        rowFound = true;
                     }
                 }
+                l += 2;
+                r += 2;
+            }
+            UInt64 rightx = endcorner;
+            UInt64 leftx = endcorner - (endcorner - 1) + pad;
+            UInt64 bottomy = endcorner;
+            UInt64 topy = endcorner - (endcorner - 1) + pad;
+            UInt64 brcorn = highsquare * highsquare;
+            UInt64 blcorn = brcorn - (1 * (highsquare - 1));
+            UInt64 tlcorn = brcorn - (2 * (highsquare - 1));
+            UInt64 trcorn = brcorn - (3 * (highsquare - 1));
+            if (num >= blcorn)
+            {
+                Console.WriteLine("({0}, {1})", rightx - (brcorn - num), bottomy);
+            }
+            else if (num >= tlcorn)
+            {
+                Console.WriteLine("({0}, {1})", leftx, bottomy - (blcorn - num));
+            }
+            else if (num >= trcorn)
+            {
+                Console.WriteLine("({0}, {1})", leftx + (tlcorn - num), topy);
+            }
+            else
+            {
+                Console.WriteLine("({0}, {1})", rightx, bottomy - (trcorn - num));
             }
         }
     }
